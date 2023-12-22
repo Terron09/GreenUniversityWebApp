a62849297ff8da4cab9a711a4b9e18112b39ee7d
OperianoECO <140956171+OperianoECO@users.noreply.github.com>
2023-12-22 23:17:36 +0800

    Created base html/py files
    Added base HTML/Python files used for coding; this is done with reference to Miro and the practicals with similar formatting convention.

--- blogClass.py
+++ blogClass.py
@@ -1,42 +1,43 @@
 # base class code
 class Blog:
-  blog_id = 0
-  def __init__(self, account, post_name, post_content, upvote_count, genre):
-    Blog.blog_id += 1
-    self.__blog_id = Blog.blog_id
-    self.__account = account
-    self.__post_name = post_name
-    self.__post_content = post_content
-    self.__upvote_count = upvote_count
-    self.__genre = genre
+    blog_id = 0
 
-  def set_account(self, account):
-      self.__account = account
+    def __init__(self, account, blog_subject, blog_content, upvote_count, genre):
+        Blog.blog_id += 1
+        self.__blog_id = Blog.blog_id
+        self.__account = account
+        self.__post_name = blog_subject
+        self.__post_content = blog_content
+        self.__upvote_count = upvote_count
+        self.__genre = genre
 
-  def set_post_name(self, post_name):
-      self.__post_name = post_name
+    def set_account(self, account):
+        self.__account = account
 
-  def set_post_content(self, post_content):
-      self.__post_content = post_content
+    def set_post_name(self, post_name):
+        self.__post_name = post_name
 
-  def set_upvote_count(self, upvote_count):
-      self.__upvote_count = upvote_count
+    def set_post_content(self, post_content):
+        self.__post_content = post_content
 
-  def set_genre(self, genre):
-      self.__genre = genre
+    def set_upvote_count(self, upvote_count):
+        self.__upvote_count = upvote_count
 
-  def get_account(self):
-      return self.__account
+    def set_genre(self, genre):
+        self.__genre = genre
 
-  def get_post_name(self):
-      return self.__post_name
+    def get_account(self):
+        return self.__account
 
-  def get_post_content(self):
-      return self.__post_content
+    def get_post_name(self):
+        return self.__post_name
 
-  def get_upvote_count(self):
-      return self.__upvote_count
+    def get_post_content(self):
+        return self.__post_content
 
-  def get_genre(self):
-      return self.__genre
-    
+    def get_upvote_count(self):
+        return self.__upvote_count
+
+    def get_genre(self):
+        return self.__genre
+
