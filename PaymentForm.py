f3776c3c935e73bf2620f071c4b9cd9f68129b62
Joshua <137512352+HackerJ0sh@users.noreply.github.com>
2024-01-03 21:51:44 +0800

    Added Venv On PC windows

--- PaymentForm.py
+++ PaymentForm.py
@@ -4,4 +4,3 @@ from wtforms import RadioField, EmailField, StringField, SelectField, TextAreaFi
 
 class CreatePaymentForm(Form):
     card_holder_name = StringField('Card Holder Name', [validators.length(min=1, max=150), validators.DataRequired()])
-    
