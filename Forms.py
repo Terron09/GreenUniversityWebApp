2e52a1a352a3bcf9b59f68d9eddf167c965c2147
TerronnnLoh <116735698+Terron09@users.noreply.github.com>
2024-01-24 14:21:36 +0800

    24/1 Update
    File Update 24 January 2024

--- Forms.py
+++ Forms.py
@@ -1,19 +1,7 @@
-from wtforms import Form, StringField, RadioField, SelectField, TextAreaField, validators
-from wtforms.fields import EmailField, DateField
-
+from wtforms import Form, RadioField, TextAreaField, validators
 class CreateUserForm(Form):
-    first_name = StringField('First Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    last_name = StringField('Last Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    gender = SelectField('Gender', [validators.DataRequired()], choices=[('', 'Select'), ('F', 'Female'), ('M', 'Male')], default='')
-    membership = RadioField('Membership', choices=[('F', 'Fellow'), ('S', 'Senior'), ('P', 'Professional')], default='F')
-    remarks = TextAreaField('Remarks', [validators.Optional()])
-
-class CreateCustomerForm(Form):
-    first_name = StringField('First Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    last_name = StringField('Last Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    gender = SelectField('Gender', [validators.DataRequired()], choices=[('', 'Select'), ('F', 'Female'), ('M', 'Male')], default='')
-    email = EmailField('Email', [validators.Email(), validators.DataRequired()])
-    date_joined = DateField('Date Joined', format='%Y-%m-%d')
-    address = TextAreaField('Mailing Address', [validators.length(max=200), validators.DataRequired()])
-    membership = RadioField('Membership', choices=[('F', 'Fellow'), ('S', 'Senior'), ('P', 'Professional')], default='F')
-    remarks = TextAreaField('Remarks', [validators.Optional()])
+#To change attributes in Create Feedback HTML
+    membership = RadioField('Satisfaction Level', choices=[('G', 'Good'), ('N', 'Neutral'), ('B', 'Bad')], default='F',
+    render_kw={'class': 'custom-radio-input'}
+)
+    remarks = TextAreaField('Feedback', [validators.Optional(), validators.length(min=1, max=500)])
